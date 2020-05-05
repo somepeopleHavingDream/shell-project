2e49574a2861b252f780dab67997839fb1f3ec24
yangxin <1393003255@qq.com>
2020-05-05 16:12:23 +0800

    feat: Shell脚本之场景控制脚本（下）

--- monitor/monitor_man.sh
+++ monitor/monitor_man.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 resettem=$(tput sgr0)
-declare -A ssharray
+declare -A sharray
 i=0
 numbers=""
 
 for script_file in `ls -I "monitor_man.sh" ./`
 do
 	echo -e '\e[1;35m' "The script:" ${i} '==>' ${resettem} ${script_file}
+	sharray[$i]=${script_file}
+	numbers="${numbers} | ${i}"
 	i=$((++i))
+done
+
+while true
+do
+	read -p "Please input a number [ ${numbers} ]:" execshell
+	if [[ ! ${execshell} =~ ^[0-9]+ ]]; then
+		exit 0
+	fi
+
+	/bin/bash ./${sharray[$execshell]}
 done
